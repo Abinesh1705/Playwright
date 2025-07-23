e826f275baa36d918c0268b0de82ddd0d22dce2d
Abinesh1705 <abineshofficial170502@gmail.com>
2025-07-23 15:37:31 +0530

    endtoedn testing

---
+++ tests/testing.spec.ts
@@ -0,0 +1,12 @@
+import { test} from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+import { users } from './pages/credentials';
+
+test("Login Testing", async({page})=> {
+
+    for (let user of users) {
+        const login = new LoginPage(page);
+        await login.goto();
+        await login.login(user.username, user.password)
+    };
+})
